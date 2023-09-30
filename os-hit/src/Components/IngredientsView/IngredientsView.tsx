e5a104db4b8369b25a318cabecf697c35494ad23
Max Shroyer <91408516+Shark9y@users.noreply.github.com>
2023-09-30 16:46:28 -0500

    fin ingredient pick

--- os-hit/src/Components/IngredientsView/IngredientsView.tsx
+++ os-hit/src/Components/IngredientsView/IngredientsView.tsx
@@ -1,4 +1,4 @@
-import { Grid, Container, Typography } from '@mui/material'
+import { Grid, Container, Typography, Button } from '@mui/material'
 import React, { useState } from 'react'
 import { MultiSelect } from "react-multi-select-component";
 import IngredientCard from '../IngredientCard/IngredientCard';
@@ -12,24 +12,50 @@ const options = [
 ];
 
 function IngredientsView() {
-    const [ingredient, setIngredient] = useState([]);
+    const [ingredientAdd, setIngredientAdd] = useState<any>([]);
+    const [ingredientList, setIngredientList] = useState<any>([]);
+
+
+    
+    const handleSubmit = () => {
+        console.log(ingredientAdd);
+        const temp = ingredientList.concat(ingredientAdd)
+        setIngredientList(temp);
+        setIngredientAdd([]);
+    };
+
+    const handleDelete = (toDel: string) => {
+        console.log(toDel);
+        let tempArr = [...ingredientList];
+        const tempIndex = tempArr.indexOf(tempArr.find((o:any) => o.value === toDel));
+        console.log(tempIndex, tempArr);
+        tempArr.splice(tempIndex, 1);
+        setIngredientList(tempArr);
+    }
+
     return (
         <Container>
             <Grid container flexDirection={"column"}>
+                <Grid container flexDirection={"row"}>
                 <Grid item>
                     <Typography>Select Ingredients To Use</Typography>
                     <MultiSelect
                         options={options}
-                        value={ingredient}
-                        onChange={setIngredient}
+                        value={ingredientAdd}
+                        onChange={setIngredientAdd}
                         labelledBy="Select"
                         hasSelectAll={false}
                     />
                 </Grid>
+                <Button onClick={handleSubmit}>
+                        Add
+                    </Button>
+                </Grid>
+                
                 <Grid item>
-                    {ingredient.map((ingredient: any) => {
+                    {ingredientList.map((ingredient: any) => {
                         return (
-                            <IngredientCard name={ingredient.value} calories={ingredient.calories} protein={ingredient.protein} moreInfo={ingredient.info} toggled={ingredient.toggled} />
+                            <IngredientCard key={ingredient.name} name={ingredient.value} calories={ingredient.calories} protein={ingredient.protein} moreInfo={ingredient.info} toggled={ingredient.toggled} handleDelete={handleDelete}/>
                         )
                     })}
                 </Grid>
