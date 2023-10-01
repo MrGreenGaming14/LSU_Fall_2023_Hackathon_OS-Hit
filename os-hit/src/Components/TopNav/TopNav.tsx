e56a24ad508c6f4d1ef2e11463e67d9b75e49e3a
Kim Nguyen <thuyduongvnguyen@gmail.com>
2023-09-30 19:26:49 -0500

    added topnav and wizard immages

--- os-hit/src/Components/TopNav/TopNav.tsx
+++ os-hit/src/Components/TopNav/TopNav.tsx
@@ -1,35 +1,102 @@
-import { Button, Container, Grid, Typography } from '@mui/material'
+import { Button, Container, Grid, Typography, AppBar, Box, Toolbar, IconButton,
+Menu, Avatar, Tooltip, MenuItem, List, ListItem, ListItemButton, ListItemText, 
+Divider, CssBaseline, Drawer } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
 import React from 'react'
+import logo from "./zestquest.png";
 import { styled } from '@mui/system';
-    
-const TopFix = styled('div')({
-    position: 'fixed',
-    top: 0,
-    width: '100%',
-    height: 60,
-    textAlign: 'center',
-});
 
+interface Props {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window?: () => Window;
+  }
+  
+const drawerWidth = 240;
+const navItems = ['Saved Recipes'];
 
-function TopNav() {
+export default function DrawerAppBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <TopFix >
-        <Grid container flexDirection={'row'} justifyContent={'space-around'}>
-            <Grid item>
-                <Typography>
-                    Zest Quest
-                </Typography>
-            </Grid>
-            <Grid item>
-                <Button>
-                    <Typography>
-                        My Recipes
-                    </Typography>
-                </Button>
-            </Grid>
-        </Grid>
-    </TopFix>
-  )
-}
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src={logo} alt="Logo" />
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            ZestQuest
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>
 
-export default TopNav
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
